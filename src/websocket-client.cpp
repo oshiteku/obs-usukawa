54b7ab680156bc9c039d66f80b8ec84489576b8b
oshiteku <oshiteku@gmail.com>
2021-08-31 00:25:41 +0900

    add: websocket client

---
+++ src/websocket-client.cpp
@@ -0,0 +1,56 @@
+#include <obs.hpp>
+#include "websocket-client.hpp"
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+WebsocketClient::WebsocketClient() {
+    _client.set_access_channels(websocketpp::log::alevel::all);
+    _client.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+    _client.init_asio();
+    _client.set_open_handler(bind(&WebsocketClient::on_open, this, ::_1));
+    _client.set_close_handler(bind(&WebsocketClient::on_close, this, ::_1));
+    _client.set_message_handler(bind(&WebsocketClient::on_message, this, ::_1, ::_2));
+}
+
+void WebsocketClient::start() {
+    std::string uri = "ws://localhost:5555";
+
+    websocketpp::lib::error_code ec;
+    client::connection_ptr con = _client.get_connection(uri, ec);
+    if (ec) {
+        blog(LOG_ERROR, "could not create websocket connection: %s", ec.message().c_str());
+        return;
+    }
+    _client.connect(con);
+
+    _client_thread = std::thread([&]{
+        try {
+            _client.run();
+        } catch (websocketpp::exception const &e) {
+            blog(LOG_ERROR, "websocket error: %s", e.what());
+        }
+    });
+}
+
+void WebsocketClient::on_open(connection_hdl hdl) {
+    auto con = _client.get_con_from_hdl(hdl);
+    auto uri = con->get_uri()->str();
+    blog(LOG_INFO, "websocket client connected: %s", uri.c_str());
+}
+
+void WebsocketClient::on_close(connection_hdl hdl) {
+    auto con = _client.get_con_from_hdl(hdl);
+    auto uri = con->get_uri()->str();
+    blog(LOG_INFO, "websocket client disconnected: %s", uri.c_str());
+}
+
+void WebsocketClient::on_message(connection_hdl hdl, message_ptr msg) {
+    auto payload = msg->get_payload();
+    blog(LOG_INFO, "websocket message received: %s", payload.c_str());
+}
+
+WebsocketClient::~WebsocketClient() {
+}
---
+++ src/websocket-client.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+
+typedef websocketpp::client<websocketpp::config::asio_client> client;
+typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
+
+using websocketpp::connection_hdl;
+
+class WebsocketClient {
+    public:
+        WebsocketClient();
+        ~WebsocketClient();
+
+        void start();
+
+    private:
+        client _client;
+        std::thread _client_thread;
+
+        void on_open(connection_hdl);
+        void on_close(connection_hdl);
+        void on_message(connection_hdl, message_ptr);
+};
