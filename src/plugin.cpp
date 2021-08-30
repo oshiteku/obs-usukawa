205e753c16a861f9592cb080a9007e66edc48248
oshiteku <oshiteku@gmail.com>
2021-08-31 00:26:35 +0900

    add: websocket client to plugin

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -2,14 +2,19 @@
 #include <obs-module.h>
 #include "plugin.hpp"
 #include "usukawa-core.hpp"
+#include "websocket-client.hpp"
 
 OBS_DECLARE_MODULE()
 
 std::shared_ptr<UsukawaCore> _usukawa_core;
+std::shared_ptr<WebsocketClient> _websocket_client;
 
 bool obs_module_load(void) {
     _usukawa_core = std::make_shared<UsukawaCore>();
-    _usukawa_core->start();
+    _websocket_client = std::make_shared<WebsocketClient>();
+
+    // _usukawa_core->start();
+    _websocket_client->start();
 
     blog(LOG_INFO, "usukawa: module loaded");
 
